e58a9f879c4056cc9c8655d09891434fbf3509b7
Mike <mike.nichols97@gmail.com>
2019-04-18 03:44:47 -0400

    Uploaded files must be .csv and will be saved in the 'data' folder

--- src/upload.php
+++ src/upload.php
@@ -1,18 +1,14 @@
 <?php
-$target_file = "../uploads/" . basename($_FILES["fileToUpload"]["name"]);
+$target_file = "../data/" . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-var_dump($imageFileType);
-// Check if image file is a actual image or fake image
-/*
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+if($fileType === "csv") {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+        echo "Sorry, there was an error uploading your file.";
     }
+} else {
+    echo "Sorry, please upload only '.csv' files";
 }
-?>*/
+?>
